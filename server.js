631fd632e86765d420e4e33829f76ac20fb7c4c9
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-10-29 18:41:34 -0500

    created local host server port

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const util = require("util");
 
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
+// setting up server
+const app = express();
+const PORT = process.env.PORT || 1706;
 
