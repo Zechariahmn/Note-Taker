73c4a138566e726441d943928178c517b3de676d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-10-29 20:09:06 -0500

    got server up and running

--- server.js
+++ server.js
@@ -1,13 +1,63 @@
-// Dependencies
+// Import dependencies
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const util = require("util");
+const noteEntries = require("./db/db.json");
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-// setting up server
+// Import express server
 const app = express();
-const PORT = process.env.PORT || 1706;
 
+// Set the PORT
+const PORT = process.env.port || 3001;
+
+// Middleware to parse JSON and url encoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('./develop/public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteEntries.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// HTML routes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+// Sets the function to create a new note
+function newNoteEntry (body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, 'db\db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+};
+
+// Post route for submitting notes
+app.post('/api/notes', (req, res) => {
+    const newNote = newNoteEntry(req.body, noteEntries);
+    res.json(newNote);
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
