62ec0ea0099a5221612ee2c6ee1365481c37855d
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-01 15:10:56 -0500

    serve.js code

--- server.js
+++ server.js
@@ -1,22 +1,84 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
-
 const app = express();
+const allNotes = require('./db/db.json');
 
+//connection to port
 const PORT = process.env.PORT || 3001;
 
-
-
-app.use(express.static('public'));
-
+// middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
-// Route files.
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
-
-
-app.listen(PORT, () => {
-  console.log(`Server available at localhost${PORT}`);
+//connection to routes
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+//creating new note
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+
+//deleting selected note
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
+    }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
