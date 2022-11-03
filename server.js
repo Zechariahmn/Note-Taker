bf0a65041c3f066e46153c5ac8ee2ba8a8ea4853
Zechariahmn <110499007+Zechariahmn@users.noreply.github.com>
2022-11-03 07:17:22 -0500

    fixed up server.js and added a uuid for the note id's

--- server.js
+++ server.js
@@ -1,88 +1,86 @@
 //requires
 const fs = require('fs');
 const path = require('path');
+const util = require("util")
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
-const allNotes = require('./db/db.json');
 
 //localhost port
 const PORT = process.env.PORT || 3001;
 
-//middleware
-app.use(express.urlencoded({ extended: true }));
+// util function 
+const readUtility = util.promisify(fs.readFile)
+const writeUtility = util.promisify(fs.writeFile)
+
+// Set up body parsing, static, and route middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-//routes to html pages
-app.get('/api/notes', (_req, res) => {
-    res.json(allNotes);
+app.get("/api/notes", (req, res) => {
+    readUtility("./db/db.json").then((data) => res.json(JSON.parse(data)))
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
+// section writes the new note to the json file and sens back to browser
+app.post("/api/notes", (req, res) => {
+    const { title, text } = req.body
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
-
-app.post('/api/notes', (req, res) => {
-    const newNote = createNewNote(req.body, allNotes);
-    res.json(newNote);
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-    deleteNote(req.params.id, allNotes);
-    res.json(true);
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-//new notes
-function createNewNote(body, notesArray) {
-    const newNote = body;
-    if (!Array.isArray(notesArray))
-        notesArray = [];
-    
-    if (notesArray.length === 0)
-        notesArray.push(0);
-
-    body.id = notesArray[0];
-    notesArray[0]++;
-
-    notesArray.push(newNote);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    );
-    return newNote;
-}
-
-
-//deleting notes
-function deleteNote(id, notesArray) {
-    for (let i = 0; i < notesArray.length; i++) {
-        let note = notesArray[i];
-
-        if (note.id == id) {
-            notesArray.splice(i, 1);
-            fs.writeFileSync(
-                path.join(__dirname, './db/db.json'),
-                JSON.stringify(notesArray, null, 2)
-            );
-
-            break;
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
         }
-    }
-}
 
+        readUtility("./db/db.json")
+            .then((data) => {
+                const oldNotes = JSON.parse(data);
+                oldNotes.push(newNote);
+                writeUtility("./db/db.json", JSON.stringify(oldNotes))
+            })
+
+        const response = {
+            status: "success",
+            body: newNote,
+        }
+
+        res.status(201).json(response)
+    } else {
+        res.status(500).json("couldn't save note")
+    }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    readUtility("./db/db.json")
+        .then((data) => {
+            let notes = JSON.parse(data)
+
+            for (let i = 0; i < notes.length; i++) {
+                let currentNote = notes[i];
+                if (currentNote.id === req.params.id) {
+                    notes.splice(i, 1);
+                    writeUtility("./db/db.json", JSON.stringify(notes))
+                    return res.status(200).json(`${currentNote} was removed successfully`)
+                }
+            }
+            return res.status(500).json("could not delete note")
+        })
+});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// If no matching route is found, then default to home
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 
 app.listen(PORT, () =>
